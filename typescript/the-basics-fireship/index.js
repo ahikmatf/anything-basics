09d348248264b627127e629df49f07c2be58d247
Hikmat <asephikmatf@gmail.com>
2020-10-24 13:11:40 +0700

    [ts] add lib config so it'll have inline docs hint

--- typescript/the-basics-fireship/index.js
+++ typescript/the-basics-fireship/index.js
@@ -2,3 +2,4 @@ console.log("first line of ts");
 async function hello() {
     return "world arround";
 }
+let url = new URL("https://google.com");
--- typescript/the-basics-fireship/index.ts
+++ typescript/the-basics-fireship/index.ts
@@ -3,3 +3,5 @@ console.log("first line of ts")
 async function hello() {
     return "world arround"
 }
+
+let url = new URL("https://google.com")
