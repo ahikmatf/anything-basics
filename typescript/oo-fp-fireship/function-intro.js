fe07c46ea60704469bf1cab9f0f89ac64c807d5d
Hikmat <asephikmatf@gmail.com>
2020-10-25 17:05:39 +0700

    [ts] make it not esnext

--- typescript/oo-fp-fireship/function-intro.js
+++ typescript/oo-fp-fireship/function-intro.js
@@ -6,24 +6,24 @@
  * 4. functions as return value
  */
 // pure functions
-let num = 123;
+var num = 123;
 function toStringFromAny(val) {
     return val.toString();
 }
-const str = toStringFromAny(num);
+var str = toStringFromAny(num);
 console.log(typeof str); // should be string
 // immutable data
-const data = Object.freeze([1, 2, 3, 4, 5, 6]);
+var data = Object.freeze([1, 2, 3, 4, 5, 6]);
 // function as arguments
-const addEmoji = (val) => toStringFromAny(val) + "😄"; // first order func, => 1😄, 2😄, etc
-const emojiData = data.map(addEmoji); // higher order function, function with function as param
+var addEmoji = function (val) { return toStringFromAny(val) + "😄"; }; // first order func, => 1😄, 2😄, etc
+var emojiData = data.map(addEmoji); // higher order function, function with function as param
 console.log(emojiData); // [ "1😄", "2😄", "3😄", "4😄", "5😄", "6😄"]
 // functions as return value
-const appendEmoji = (fixed) => (dynamic) => fixed + dynamic;
+var appendEmoji = function (fixed) { return function (dynamic) { return fixed + dynamic; }; };
 // rain is the function returned, dynamic => fixed + dynamic
 // "🌧️" is the fixed argument
-const rain = appendEmoji("🌧️");
-const sun = appendEmoji("🌞");
+var rain = appendEmoji("🌧️");
+var sun = appendEmoji("🌞");
 // well, i get the idea, but still can't put it in words, so here
 console.log(rain("today")); // 🌧️today
 console.log(sun("tomorrow")); // 🌞tomorrow
