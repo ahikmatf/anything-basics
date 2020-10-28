8948ffe0e50a18b6f73d6853453b52f6e200416f
Hikmat <asephikmatf@gmail.com>
2020-10-28 08:13:22 +0700

    [swft] learn about type annotations

---
+++ swiftui/hundred-days-of-swift-twostraws/hundred-days-of-swift-twostraws/Part1/Day1/TypeAnnotations.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var str = "Hello, playground" // this will be string, type inference
+
+// explicit type
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+// int is for integer
+// bool is for boolean
