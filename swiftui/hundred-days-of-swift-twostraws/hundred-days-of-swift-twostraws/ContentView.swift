a94f9efbb8d507430581cc510462b5db86fdbc02
Hikmat <asephikmatf@gmail.com>
2020-10-28 07:53:49 +0700

    [sui] new project of swiftui

---
+++ swiftui/hundred-days-of-swift-twostraws/hundred-days-of-swift-twostraws/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hundred-days-of-swift-twostraws
+//
+//  Created by Fatah on 28/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui/hundred-days-of-swift-twostraws/hundred-days-of-swift-twostraws/hundred_days_of_swift_twostrawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  hundred_days_of_swift_twostrawsApp.swift
+//  hundred-days-of-swift-twostraws
+//
+//  Created by Fatah on 28/10/20.
+//
+
+import SwiftUI
+
+@main
+struct hundred_days_of_swift_twostrawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
