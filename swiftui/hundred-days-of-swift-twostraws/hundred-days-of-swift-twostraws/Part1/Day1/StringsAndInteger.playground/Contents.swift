23dddfc93ff14c147c7fe7d78a05c838242367e0
Hikmat <asephikmatf@gmail.com>
2020-10-28 08:02:37 +0700

    [swft] learn integer

---
+++ swiftui/hundred-days-of-swift-twostraws/hundred-days-of-swift-twostraws/Part1/Day1/StringsAndInteger.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var age = 38
+var population = 8_000_000 // underscore can be used as thousand separator for int
+
+// age = "thirty-eight" - caused error because it has different type
