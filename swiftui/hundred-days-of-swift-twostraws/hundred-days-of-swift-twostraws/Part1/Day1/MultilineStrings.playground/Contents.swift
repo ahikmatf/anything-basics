29f42fc216789af5b4804c856336d8006933aa17
Hikmat <asephikmatf@gmail.com>
2020-10-28 08:05:38 +0700

    [swft] learn multiline strings

---
+++ swiftui/hundred-days-of-swift-twostraws/hundred-days-of-swift-twostraws/Part1/Day1/MultilineStrings.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+// the triple quote must be on their own line
+var str1 = """
+this goes
+over multiple
+lines
+"""
+
+// only breaks in code, not in result, just add backslash
+var str2 = """
+this goes \
+over multiple \
+lines
+"""
