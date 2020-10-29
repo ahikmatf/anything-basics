3631d6b14e97f24a1f201c56834e8d4044867197
Hikmat <asephikmatf@gmail.com>
2020-10-30 01:29:04 +0700

    [sui] create meme view item

---
+++ swiftui/meme-generator-swlh/MemeGenerator/MemeGenerator/MemeView/MemeView.swift
@@ -0,0 +1,26 @@
+//
+//  MemeView.swift
+//  MemeGenerator
+//
+//  Created by Fatah on 30/10/20.
+//
+
+import SwiftUI
+
+struct MemeView: View {
+    
+    // the individual meme info
+    let meme: Meme.Info
+    
+    var body: some View {
+        
+        // an image loaded from url
+        AsyncImage()
+    }
+}
+
+struct MemeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MemeView()
+    }
+}
