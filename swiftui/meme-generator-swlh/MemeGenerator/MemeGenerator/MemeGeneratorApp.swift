cca38a17fb7d92da95a32a2df5370855da09392d
Hikmat <asephikmatf@gmail.com>
2020-10-29 17:40:56 +0700

    [sui] init meme generator project

---
+++ swiftui/meme-generator-swlh/MemeGenerator/MemeGenerator/MemeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeGeneratorApp.swift
+//  MemeGenerator
+//
+//  Created by Fatah on 29/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MemeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
