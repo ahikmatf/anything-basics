ba5e4ed1e1ac2a36dca94fe9991bb7544a4c60f4
Hikmat <asephikmatf@gmail.com>
2020-10-30 09:45:10 +0700

    [sui] create async image loader

--- swiftui/meme-generator-swlh/MemeGenerator/MemeGenerator/MemeView/MemeView.swift
+++ swiftui/meme-generator-swlh/MemeGenerator/MemeGenerator/MemeView/MemeView.swift
@@ -15,12 +15,37 @@ struct MemeView: View {
     var body: some View {
         
         // an image loaded from url
-        AsyncImage()
+        AsyncImage(url: URL(string: meme.url ?? "https://i.imgflip.com/1bij.jpg")!) {
+            RoundedRectangle(cornerRadius: 0)
+        }
+        .aspectRatio(contentMode: .fill) // fill the available space
+        .frame(width: UIScreen.main.bounds.width - 32, // width of screen
+               height: 350)
+        .overlay(
+            // overlay to add text at the bottom of the image, with gradient background to make it readable
+            Text(meme.name ?? "")
+                .bold()
+                .font(.system(size: 27,
+                              weight: .bold,
+                              design: .rounded))
+                .foregroundColor(.white)
+                .lineLimit(3)
+                .frame(maxHeight: .infinity, alignment: .leading)
+                .padding(.leading, 10)
+                .padding(.bottom, 10)
+                .background(
+                    LinearGradient(gradient: Gradient(colors: [Color.black.opacity(0.75), .clear]),
+                                   startPoint: .bottom,
+                                   endPoint: .top)
+                ), alignment: .bottom
+        )
+        .cornerRadius(20)
+        .padding(.vertical)
     }
 }
 
 struct MemeView_Previews: PreviewProvider {
     static var previews: some View {
-        MemeView()
+        MemeView(meme: .fakeMeme)
     }
 }
